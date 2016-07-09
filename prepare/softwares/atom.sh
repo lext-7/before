80bd70553b0335bb7b9908d37effd8f3ce6f8275
lext <lext.nobody@outlook.com>
2016-07-09 08:48:39 +0800

    add atom install sh

---
+++ prepare/softwares/atom.sh
@@ -0,0 +1,18 @@
+
+apm install atom-beautify
+apm install docblockr
+apm install highlight-selected
+apm install linter-eslint
+apm install atom-terminal
+apm install emmet
+apm install hyperclick
+apm install minimap
+apm install atom-ternjs
+apm install file-icons
+apm install js-hyperclick
+apm install open-recent
+apm install path-hyperclick
+apm install git-control
+apm install autocomplete-paths
+apm install
+apm install

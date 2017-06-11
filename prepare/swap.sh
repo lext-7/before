2019b28b5916fbda1a8f57e47a37127910854a0e
lext <lext.nobody@outlook.com>
2017-06-11 12:25:58 +0800

    swap generate

--- prepare/swap.sh
+++ prepare/swap.sh
@@ -2,16 +2,21 @@
 # run as root
 
 size=$1
+filename=$2
 
 if [ -z "$size" ]; then
     read -p "set your swap size(M): " size
+fi
+
+if [ -z "$filename" ]; then
+    filename=/swap
 fi
 
 if [ -z "$size" ]; then
     size=1024
 fi
 
-sudo dd if=/dev/zero of=/swap bs=$size count=1M
-sudo mkswap /swap
-sudo swapon /swap
-sudo -u root echo "/swap swap swap sw 0 0" >> /etc/fstab
+sudo dd if=/dev/zero of=$filename bs=$size count=1M
+sudo mkswap $filename
+sudo swapon $filename
+su -c 'echo "$filename swap swap sw 0 0" >> /etc/fstab'
